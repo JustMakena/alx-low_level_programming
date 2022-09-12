2c164fbc66cd875ef00bbc917ca44afcf9ed4c07
JustMakena <justmakenan@gmail.com>
2022-09-12 07:39:53 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,9 +4,9 @@
 
 
 /**
-* main - prints all single digit numbers of base
+* main - prints all single digit numbers of base 10 starting from 0,
 *
-* 10 starting from 0, followed by a new line
+* followed by a new line, using putchar
 *
 * Return: Always 0 (Success)
 */
@@ -14,8 +14,10 @@
 int main(void)
 {
 int n;
-for (n = 0; n < 10; n++)
-printf("%d", n);
-printf("\n");
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
 return (0);
 }

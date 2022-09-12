50ac7f5b759ed2d229c2f2aed27a5afbd708700e
JustMakena <justmakenan@gmail.com>
2022-09-12 14:41:45 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Prints the phrase "Programming is like a puzzle"
- *
- *Return 0 if exited properly, non-zero otherwise
- */
+*main - Prints the phrase "Programming is like a puzzle"
+*
+*Return 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
-  puts("Programming is like a puzzle");
-  return (0);
+puts("Programming is like a puzzle");
+return (0);
 }

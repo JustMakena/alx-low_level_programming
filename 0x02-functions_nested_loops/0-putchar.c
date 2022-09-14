41147e49d1bfd7fdbecda08e7fa79c677c8bcb26
JustMakena <justmakenan@gmail.com>
2022-09-14 02:37:31 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* Main - Program that prints _putchar
+* Followed by a new line
+* Return 0 if properly executed, non zero otherwise
+*/
+
+
+int main(void)
+{
+printf(""_"putchar, \n");
+return (0);
+}

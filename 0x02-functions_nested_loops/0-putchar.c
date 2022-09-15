bd4203ee9466dc95970d73e7d48c1810c54dcb36
JustMakena <justmakenan@gmail.com>
2022-09-15 03:50:52 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,41 @@
 #include "main.h"
 
 /**
-* Main - prints _putchar
-*
-* Description: prints _putchar
-*
-*Return 0 (Success)
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
 
 int main(void)
+  
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+  
+  _putchar('_');
+  
+  _putchar('p');
+  
+  _putchar('u');
+  
+  _putchar('t');
+  
+  _putchar('c');
+  
+  _putchar('h');
+  
+  _putchar('a');
+  
+  _putchar('r');
+  
+  _putchar('\n');
+  
 
-return (0);
+  
+  return (0);
+  
+
+  
 }

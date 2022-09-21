eff13ad0a4bfabb82a03039e1feb7db824dc462e
JustMakena <justmakenan@gmail.com>
2022-09-20 17:47:32 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,16 @@
 #include "main.h"
-
 /**
-* _puts - prints a string, followed by a new line.
-* @str: input string.
-* Return: no return.
+* _puts - prints a string, followed by a new line
+* @str: input string
+* Return: no thing
 */
-
 void _puts(char *str)
 {
-int count = 0;
-while (count >= 0)
+int i = 0;
+while (*(str + i))
 {
-if (str[count] == '\0')
-{
+_putchar(*(str + i));
+i++;
+}
 _putchar('\n');
-break;
-}
-_putchar(str[count]);
-count++;
-}
 }

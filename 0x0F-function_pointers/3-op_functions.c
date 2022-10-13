680bd7435494938529d8d7470e3ac5379a23694f
JustMakena <justmakenan@gmail.com>
2022-10-13 15:53:31 -0700

    try

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,11 @@
 #include "3-calc.h"
 
 /**
-* op_add - funtion to add
-* @a: input 1
-* @b: input 2
-* Return: sum a and b
+* op_add - adds two numbers.
+* @a: first number.
+* @b: second number.
+*
+* Return: add.
 */
 
 int op_add(int a, int b)
@@ -13,10 +14,11 @@ return (a + b);
 }
 
 /**
-* op_sub - funtion to subtract
-* @a : input 1
-* @b: input 2
-* Return: difference of a and b
+* op_sub - subctracts two numbers.
+* @a: first number.
+* @b: second number.
+*
+* Return: difference.
 */
 
 int op_sub(int a, int b)
@@ -25,10 +27,11 @@ return (a - b);
 }
 
 /**
-* op_mul - funtion to multiplies
-* @a: input 1
-* @b: input 2
-* Return: product of a and b
+* op_mul - multiplies two numbers.
+* @a: first number.
+* @b: second number.
+*
+* Return: multiplication.
 */
 
 int op_mul(int a, int b)
@@ -37,85 +40,37 @@ return (a * b);
 }
 
 /**
-* op_div - function to div
-* @a: input 1
-* @b: input 2
-* Return: division of a by b
+* op_div - divides two numbers.
+* @a: first number.
+* @b: second number.
+*
+* Return: division.
 */
 
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 
 /**
-* op_mod - function to mod
-* @a: input 1
-* @b: input 2
-* Return: remainder of the division of a
+* op_mod - calculates the module of two numbers.
+* @a: first number.
+* @b: second number.
+*
+* Return: remainder of the division.
 */
 
 int op_mod(int a, int b)
 {
-return (a % b);
-}#include "3-calc.h"
-
-/**
-* op_add - funtion to add
-* @a: input 1
-* @b: input 2
-* Return: sum a and b
-*/
-
-int op_add(int a, int b)
+if (b == 0)
 {
-return (a + b);
+printf("Error\n");
+exit(100);
 }
-
-/**
-* op_sub - funtion to subtract
-* @a : input 1
-* @b: input 2
-* Return: difference of a and b
-*/
-
-int op_sub(int a, int b)
-{
-return (a - b);
-}
-
-/**
-* op_mul - funtion to multiplies
-* @a: input 1
-* @b: input 2
-* Return: product of a and b
-*/
-
-int op_mul(int a, int b)
-{
-return (a * b);
-}
-
-/**
-* op_div - function to div
-* @a: input 1
-* @b: input 2
-* Return: division of a by b
-*/
-
-int op_div(int a, int b)
-{
-return (a / b);
-}
-
-/**
-* op_mod - function to mod
-* @a: input 1
-* @b: input 2
-* Return: remainder of the division of a
-*/
-
-int op_mod(int a, int b)
-{
 return (a % b);
 }

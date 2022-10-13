f433f579b0774cd2723ead05d3881ccd01c81434
JustMakena <justmakenan@gmail.com>
2022-10-13 15:45:39 -0700

    changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,27 +1,19 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
-* int_index - function that searches for an integer
-* @array: input array
-* @size: number of elements in the array
-* @cmp: pointer to the function to be used to compare values
-* Return: int
+* array_iterator - executes a function given as a
+* parameter on each element of an array.
+* @array: input integer array.
+* @size: size of the array.
+* @action: pointer to the function.
+*
+* Return: no return.
 */
 
-int int_index(int *array, int size, int (*cmp)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
-if (array != 0 && cmp != 0)
-{
-if (size <= 0)
-return (-1);
+unsigned int i;
+if (array && action)
 for (i = 0; i < size; i++)
-{
-if (cmp(array[i]))
-return (i);
-}
-}
-/* If element not found */
-return (-1);
+action(array[i]);
 }

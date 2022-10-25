eca2e1dc5b25380c52e27a67cb237df13e2f8aa7
JustMakena <justmakenan@gmail.com>
2022-10-24 18:26:59 -0700

    hope this works

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,19 +3,18 @@
 
 /**
 * free_list - function that frees a list_t list
-* @head: input header pointer
-* Return: nothing
+* @head: A pointer to the list_t list
 */
 
 void free_list(list_t *head)
 {
 list_t *tmp;
 
-while (head != NULL)
+while (head)
 {
-tmp = head;
 head = head->next;
-free(tmp->str);
-free(tmp);
+free(head->str);
+free(head);
+head = tmp;
 }
 }

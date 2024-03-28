617b661d4eb4fc8385a590b9ddf2c8213c31fb7c
JustMakena <justmakenan@gmail.com>
2024-03-28 10:34:36 -0700

    chnage

--- 0x1C-makefiles/main.c
+++ 0x1C-makefiles/main.c
@@ -1,12 +1,12 @@
 #include "m.h"
 
 /**
- * main - Entry point
+ * main - Our Entry point
  *
  * Return: Always 
  */
 int main(void)
 {
-	print_school();
-	return (EXIT_SUCCESS);
-}
+    print_holberton();
+    return (EXIT_SUCCESS);
+}/*school */
--- 0x1C-makefiles/school.c
+++ 0x1C-makefiles/school.c
@@ -1,11 +1,10 @@
 #include "m.h"
 
 /**
- * print_school
- *
+ * print_holberton - printing holberton logo *
  * Return: Nothing
  */
-void print_school(void)
+void print_holberton(void)
 {
 	printf("j#0000000000000000000000000000000000000\n");
 	printf("j#000000000000000000@Q**g00000000000000\n");
